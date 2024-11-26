429301bc69112b630ed2f68e839cc81efc45ac8c
3BGPR012 <00001096457714sp@al.educacao.sp.gov.br>
2024-11-25 21:21:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Historia',
+    'quando começou a era das grandes navegações?',
+    'século XV'
+)
+
+criaCartao(
+    'Matematica',
+    '4+4?',
+    '4+4= 8'
+)
+
+criaCartao(
+    'Inglês',
+    'O Azul em inglês é dito como?',
+    'Blue'
+)
+
+criaCartao(
+    'Historia',
+    'Quem descobriu o Brasil?',
+    'Pedro Alvares Cabral'
+)
